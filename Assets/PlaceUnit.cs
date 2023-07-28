44f21ddc11bde364fbe50ccd329c00b6892d1508
TheFakeThanos <EerdekensInc@gmail.com>
2023-07-28 23:00:50 +0200

    added unit selection

--- Assets/PlaceUnit.cs
+++ Assets/PlaceUnit.cs
@@ -17,7 +17,6 @@ public class PlaceUnit : MonoBehaviour
     public Transform HologramPoint;
     public GameObject RightController;
 
-    public LayerMask placeableMask;
 
     private RaycastHit hit;
     private RaycastHit hit2;
@@ -29,25 +28,24 @@ public class PlaceUnit : MonoBehaviour
     private GameObject SelectedObj;
     private MeshRenderer selectedMesh;
 
+    private GameObject Hull;
+
     // Start is called before the first frame update
     void Start()
     {
         rightTriggerAction.action.performed += spawnUnity;
-
-        ManaMangerScript.UpdateCost(obj.Cost);
-        Unit = obj.Object;
-        SelectedObj = Instantiate(obj.ObjectHull, Vector3.zero, Quaternion.identity);
-        selectedMesh = SelectedObj.GetComponent<MeshRenderer>();
     }
 
     private void spawnUnity(InputAction.CallbackContext _obj)
     { 
-        if(Physics.Raycast(RightController.transform.position, RightController.transform.forward, out hit, 1000f, placeableMask) && ManaMangerScript.CurrMana > obj.Cost)
+        if(Physics.Raycast(RightController.transform.position, RightController.transform.forward, out hit, 1000f) && ManaMangerScript.CurrMana > obj.Cost)
         {
-            GameObject tmp = Instantiate(Unit, hit.point + new Vector3(0, Unit.transform.localScale.y/2, 0), Quaternion.identity);
-            tmp.GetComponent<HealthScript>().Team = MyTeam;
-            tmp.layer = gameObject.layer;
-            ManaMangerScript.RemoveMana(obj.Cost);
+            if(hit.collider.tag == "Placable"){
+                GameObject tmp = Instantiate(Unit, hit.point + new Vector3(0, Unit.transform.localScale.y / 2, 0), Quaternion.identity);
+                tmp.GetComponent<HealthScript>().Team = MyTeam;
+                tmp.layer = gameObject.layer;
+                ManaMangerScript.RemoveMana(obj.Cost);
+            }
         }
     }
 
@@ -55,8 +53,17 @@ public class PlaceUnit : MonoBehaviour
     {
         if(Physics.Raycast(RightController.transform.position, RightController.transform.forward, out hit2))
         {
-            SelectedObj.transform.position = hit2.point + new Vector3(0, SelectedObj.transform.localScale.y/2, 0);
-            if (((1<<hit2.collider.gameObject.layer) & placeableMask) != 0) // ????????????????????????????????????????? who made this shit
+            if(hit2.collider.tag != "noDisplay")
+            {
+                SelectedObj.transform.position = hit2.point + new Vector3(0, SelectedObj.transform.localScale.y / 2, 0);
+            } 
+            else if(hit2.collider.tag == "noDisplay")
+            {
+                SelectedObj.transform.position = HologramPoint.position;
+            }
+
+            
+            if (hit2.collider.tag == "Placable")
             {
                 if(ManaMangerScript.CurrMana >= obj.Cost && selectedMesh.material != PlaceableMat)
                 {
@@ -80,8 +87,9 @@ public class PlaceUnit : MonoBehaviour
             }
             SelectedObj.transform.position = HologramPoint.position;
         }
-        if (Physics.Raycast(RightController.transform.position, RightController.transform.forward, 1000f, placeableMask))
+        if (Physics.Raycast(RightController.transform.position, RightController.transform.forward, out hit2,1000f))
         {
+            if(hit2.collider.tag == "Placable")
             ManaMangerScript.UpdateCost(obj.Cost);
         }
         else
@@ -89,6 +97,20 @@ public class PlaceUnit : MonoBehaviour
             ManaMangerScript.UpdateCost(0);
         }
 
+    }
+
+    public void SelectNew(ObjectHolder newObj)
+    {
+        if (obj)
+        {
+            Destroy(Hull);
+        }
+
+        obj = newObj;
+        ManaMangerScript.UpdateCost(newObj.Cost);
+        Unit = obj.Object;
+        Hull = SelectedObj = Instantiate(newObj.ObjectHull, Vector3.zero, Quaternion.identity);
+        selectedMesh = SelectedObj.GetComponent<MeshRenderer>();
     }
 
     private void OnDestroy()
