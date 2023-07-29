51806a91ddeee49bd3c39e5c9538aa5e062c148e
TheFakeThanos <EerdekensInc@gmail.com>
2023-07-29 18:51:43 +0200

    Update DisableNotLocal.cs

--- Assets/DisableNotLocal.cs
+++ Assets/DisableNotLocal.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class DisableNotLocal : MonoBehaviourPunCallbacks
 {
     public GameObject[] objList;
-    public override void OnJoinedRoom()
+    public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        base.OnJoinedRoom();
+        base.OnPlayerEnteredRoom(newPlayer);
         if (!photonView.IsMine)
         {
             foreach (GameObject obj in objList)
