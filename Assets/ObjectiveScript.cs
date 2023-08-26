a896ff55a59bc4909c86f8a8939ffbac4962c42d
TheFakeThanos <EerdekensInc@gmail.com>
2023-08-26 19:27:09 +0200

    objective functionality

--- Assets/ObjectiveScript.cs
+++ Assets/ObjectiveScript.cs
@@ -1,12 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Photon.Pun;
 
 public class ObjectiveScript : MonoBehaviour
 {
+    private PhotonView view;
+    public GameObject GameEndText;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
     private void OnDestroy()
     {
-        Debug.Log(tag + " lost");
-        //load victory screen
+        if (view.IsMine)
+        {
+            view.RPC("TeamLost", RpcTarget.AllBuffered, tag);
+        }
+    }
+
+    [PunRPC]
+    private void TeamLost(string lossTag)
+    {
+        GameObject tmp = Instantiate(GameEndText, Vector3.up * 10, Quaternion.identity);
+        if (lossTag == tag)
+        {
+            tmp.GetComponent<TextMeshPro>().text = "You lost.";
+        }
+        else if(lossTag != tag)
+        {
+            tmp.GetComponent<TextMeshPro>().text = "You won.";
+        }
     }
 }
